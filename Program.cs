c89a8039e23c486607fd1ccda16a4f4a06e81984
tayfunerkorkmaz <tayfun.erkorkmaz@samm.com>
2021-07-21 15:28:41 +0300

    dönüşümler eklendi

--- Program.cs
+++ Program.cs
@@ -25,13 +25,34 @@ namespace csharp_dotnet5_consoleapp_2
             object o1 = 4.3;
             object o2 = "test";
 
-            int value1= 90;
-            string value2 = Convert.ToString(value1);
-            string value3 = value1.ToString();
-            
-            bool bVal = 1>5;
+
+
+            bool bVal = 1 > 5;
             Console.WriteLine(bVal);
 
+            //Dönüşümler
+
+            Console.WriteLine("***Implicit Conversion"); //Bilinçsiz Dönüşüm
+
+            int iValx = 5;
+            string kValx = "Merhaba" + iValx;
+
+            Console.WriteLine("***Explicit Conversion"); //Bilinçli Dönüşüm
+            float fVal2 = 1.4f;
+            byte bVal2 = (byte)fVal2;
+
+            Console.WriteLine("***ToString() Conversion"); //ToString Dönüşüm
+            int value1 = 90;
+            string value2 = Convert.ToString(value1);
+            string value3 = value1.ToString();
+
+            Console.WriteLine("***System.Convert Class Conversion"); //System.Convert Class  Dönüşüm
+            string sVal = "5";
+            int intVal = Convert.ToInt32(sVal);
+
+            Console.WriteLine("***Parse Method Conversion"); //Parse Method Dönüşümü
+            string sVal2 = "10.1";
+            float fVal3 = float.Parse(sVal2);
 
         }
     }
